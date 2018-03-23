e59212abf996bbf0c128d825f85fdda1a9a11578
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-03-23 16:28:01 +0530

    Create sri72.c

---
+++ sri72.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+char c[10];
+int i,count=0;
+printf("enter the STRING \n");
+scanf("%c",&c[i]);
+for(i=0;i<5;i++)
+if(c[i]=='a'||c[i]=='A'||c[i]=='e'||c[i]=='E'||c[i]=='i'||c[i]=='I'||c[i]=='o'||c[i]=='O'||c[i]=='u'||c[i]=='U')
+{
+count++;
+}
+if(count!=0)
+{
+
+printf("it has vowel");
+}
+else
+{
+printf("it has no vowel");
+}
+return 0;
+}
