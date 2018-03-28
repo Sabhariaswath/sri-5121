54c996d56ffac100e04cb9a74f7fbbc9847b3870
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-03-28 03:20:38 -0700

    Create sri75.c

---
+++ sri75.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+{
+char str[10];
+int n;
+printf("enter the string:");
+scanf("%s",&str);
+n=strlen(str);
+if(n%2==0)
+{
+str[(n/2-1)]='*';
+str[n/2]='*';
+}
+else
+{
+str[n/2]='*';
+}
+printf("%s",str);
+return 0;
+}
